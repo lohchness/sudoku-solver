2bf47fc1884d95870f3ab2611e20b0092530fcec
Lok-Hymn Chow <50405970+lohchness@users.noreply.github.com>
2021-11-23 23:14:52 +0800

    edits for privacy

--- sudoku-solver.c
+++ sudoku-solver.c
@@ -1,45 +1,3 @@
-/* Program to assist in the challenge of solving sudoku puzzles.
-
-   Skeleton program written by Alistair Moffat, ammoffat@unimelb.edu.au,
-   August 2021, with the intention that it be modified by students
-   to add functionality, as required by the assignment specification.
-
-   Student Authorship Declaration:
-
-   (1) I certify that except for the code provided in the initial skeleton
-   file, the  program contained in this submission is completely my own
-   individual work, except where explicitly noted by further comments that
-   provide details otherwise.  I understand that work that has been developed
-   by another student, or by me in collaboration with other students, or by
-   non-students as a result of request, solicitation, or payment, may not be
-   submitted for assessment in this subject.  I understand that submitting for
-   assessment work developed by or in collaboration with other students or
-   non-students constitutes Academic Misconduct, and may be penalized by mark
-   deductions, or by other penalties determined via the University of
-   Melbourne Academic Honesty Policy, as described at
-   https://academicintegrity.unimelb.edu.au.
-
-   (2) I also certify that I have not provided a copy of this work in either
-   softcopy or hardcopy or any other form to any other student, and nor will I
-   do so until after the marks are released. I understand that providing my
-   work to other students, regardless of my intention or any undertakings made
-   to me by that other student, is also Academic Misconduct.
-
-   (3) I further understand that providing a copy of the assignment
-   specification to any form of code authoring or assignment tutoring service,
-   or drawing the attention of others to such services and code that may have
-   been made available via such a service, may be regarded as Student General
-   Misconduct (interfering with the teaching activities of the University
-   and/or inciting others to commit Academic Misconduct).  I understand that
-   an allegation of Student General Misconduct may arise regardless of whether
-   or not I personally make use of such solutions or sought benefit from such
-   actions.
-
-   Signed by: Lok-Hymn Chow 1245550
-   Dated:     1/9/2021
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -633,5 +591,3 @@ void ta_daa(int cell_count) {
 }
 
 /****************************************************************/
-
-/* Algorithms are fun :) */
